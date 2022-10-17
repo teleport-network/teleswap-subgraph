3d016324797818834bf49dd1ef15d637c24fee28
jooyyy <joy060102@gmail.com>
2022-10-17 12:33:11 +0800

    update address

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -8,7 +8,7 @@ import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 import { TokenDefinition } from './tokenDefinition'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x5ABc39891c04B5e1c4a80b76a785b706B84F4528'
+export const FACTORY_ADDRESS = '0x32e12eefB978af0BcC1a5E23d5069B59cb3A590b'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
